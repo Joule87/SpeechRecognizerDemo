42c5fd97a12d9fb7430a72b831683b6cdb8aff24
Julio Collado <jcollado@altimetrik.com>
2020-04-24 18:26:44 -0300

    SpeechRecognizerDemo: Base Project

--- SpeechRecognizerDemo/ViewController.swift
+++ SpeechRecognizerDemo/ViewController.swift
@@ -9,12 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    var recordButton: UIBarButtonItem!
+    
+    var isRecording = false
+    private let pulsingLayerName = "pulsingLayer"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupSearchBar()
+        setupRecordButton()
+    }
+    
+    func setupSearchBar() {
+        let searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search"
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+    }
+    
+    func setupRecordButton() {
+        recordButton = UIBarButtonItem(image: #imageLiteral(resourceName: "micro"), style: .done, target: self, action: #selector(didTapRecordButton))
+        recordButton.tintColor = .black
+        navigationItem.rightBarButtonItem = recordButton
+    }
+    
+    @objc func didTapRecordButton() {
+        isRecording ? stopRecordingButtonAnimation() : startRecordingButtonAnimation()
+        isRecording = !isRecording
+    }
+    
+    func stopRecordingButtonAnimation() {
+        navigationController?.view.layer.sublayers?.removeAll{ $0.name == pulsingLayerName}
+        recordButton.tintColor = .black
+    }
+    
+    func startRecordingButtonAnimation() {
+        guard let buttonView = recordButton.value(forKey: "view") as? UIView,
+            let position = buttonView.superview?.convert(buttonView.center, to: nil) else { return }
+        recordButton.tintColor = .red
+        let radius = buttonView.frame.size.height * 0.60
+        let pulse = Pulsing(radius: radius, position: position)
+        pulse.name = pulsingLayerName
+        pulse.animationDuration = 0.8
+        pulse.backgroundColor = UIColor.red.cgColor
+        navigationController?.view.layer.insertSublayer(pulse, above: buttonView.layer)
     }
 
+}
 
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        resultLabel.text = searchBar.text
+        searchBar.resignFirstResponder()
+    }
+    
+    
 }
 
